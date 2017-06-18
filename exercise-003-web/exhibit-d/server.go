70e00fd47ab851dab8bf5b52eb43cbd6ef431c7e
Mohammad Abdeljalil <mabdeljalil@enova.com>
2017-06-18 16:02:15 -0500

    WIP, Post go-lab

--- exercise-003-web/exhibit-d/server.go
+++ exercise-003-web/exhibit-d/server.go
@@ -1,25 +1,43 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"html/template"
 	"net/http"
 )
 
+type View struct {
+	nameList map[string]int
+	testy    string
+}
+
 var homeT = template.Must(template.ParseFiles("exhibit-d/home.html"))
+var v View
 
 func home(w http.ResponseWriter, r *http.Request) {
-	homeT.Execute(w, nil)
+	homeT.Execute(w, &v)
+}
+
+func addUser(name string) {
+	if v.nameList[name] >= 1 {
+		v.nameList[name]++
+	} else {
+		v.nameList = make(map[string]int)
+		v.nameList[name] = 1
+	}
+	return
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	username := r.Form.Get("username")
-	msg := "Hey " + username + ", did you try to sign-up?"
-	fmt.Fprintf(w, msg)
+
+	addUser(username)
+	http.Redirect(w, r, "/home", http.StatusFound)
 }
 
 func main() {
+	http.HandleFunc("/", home)
 	http.HandleFunc("/home", home)
 	http.HandleFunc("/signup", signup)
 	http.ListenAndServe(":8080", nil)
