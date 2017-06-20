48f81082c09b64a969186475e6aca29d43bb188e
Mohammad Abdeljalil <mabdeljalil@enova.com>
2017-06-19 23:30:28 -0500

    Added (Isocoeles) Triangle

--- exercise-008-iface/exhibit-c/shape.go
+++ exercise-008-iface/exhibit-c/shape.go
@@ -45,6 +45,27 @@ func (c *Circle) Area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
+// Triangle (Isocleles)
+
+type Triangle struct {
+	LongSide  int
+	ShortSide int
+	Height    float64
+}
+
+func (t *Triangle) Name() string {
+	return "Triangle"
+}
+
+func (t *Triangle) Perimeter() float64 {
+	return float64(t.ShortSide + t.LongSide*2)
+}
+
+func (t *Triangle) Area() float64 {
+	t.Height = math.Sqrt(math.Pow(float64(t.LongSide), 2.0) - math.Pow(0.5*float64(t.ShortSide), 2.0))
+	return float64(0.5) * float64(t.ShortSide) * float64(t.Height)
+}
+
 ////////////////
 // Efficiency //
 ////////////////
@@ -70,4 +91,7 @@ func main() {
 
 	c := Circle{radius: 10.0}
 	Efficiency(&c)
+
+	t := Triangle{LongSide: 4, ShortSide: 2}
+	Efficiency(&t)
 }
